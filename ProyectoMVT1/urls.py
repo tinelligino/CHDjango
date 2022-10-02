15616c00fded489f7e5f00e504d0f6421440c48b
tinelligino <ginortinelli@gmail.com>
2022-10-02 19:39:16 -0300

    Version2

--- ProyectoMVT1/urls.py
+++ ProyectoMVT1/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from ProyectoMVT1.ProyectoMVT1.views import bienvenida
+from .import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registrofamiliar/', views.bienvenida),
+    path('registro/', views.registro),
 ]
+
--- ProyectoMVT1/views.py
+++ ProyectoMVT1/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
-
+from django.template import loader
 def bienvenida(request):
-    return HttpResponse("Ponete a ver las clases")
+    return HttpResponse("Pablo, 25 años, Argentina, 21/01")
+
+def registro(request):
+    template = loader.get_template("template01.html")
+    diccionario = {
+        "nombre01":"Guillermo Tinelli",
+        "nombre02":"Paola Bafundo",
+        "nombre03":"Giovanni Tinelli",
+        "nombre04":"Gino Tinelli",
+        }
+    response = template.render(diccionario)
+    return HttpResponse(response)
+
